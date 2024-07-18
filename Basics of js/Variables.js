336d7001db0e7b4a3d3f46eed06cfaf421a5803d
Khushi Chhabadia <khushichhabadia16@gmail.com>
2024-07-18 18:08:48 +0530

    JS

---
+++ Basics of js/Variables.js
@@ -0,0 +1,6 @@
+var name = 'Khushi Chhabadia';
+//is a function-scoped and can be reassigned
+let age = ' 18';
+//is a blocked-scoped and can be reaasisgned
+const isStudent = true;
+//is a blocked-scoped and can not be reassigned
