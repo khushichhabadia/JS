34ac5b43c1d6602069d1d5d118ce435db27d6a3c
Khushi Chhabadia <149806346+khushichhabadia@users.noreply.github.com>
2024-08-02 20:49:56 +0530

    Update Loops.js

--- Basics of js/Loops.js
+++ Basics of js/Loops.js
@@ -1,16 +1,16 @@
-// for loop
+// for loop use when you know now many number of iterations has 5o be printed and set condition accordingly 
 for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
-// while loop
+// while loop used when you know condition but not number of iterations 
 let j = 0;
 while (j < 5) {
     console.log(j);
     j++;
 }
 
-// do-while loop
+// do-while loop used when you need output first then to check the condition 
 let k = 0;
 do {
     console.log(k);
